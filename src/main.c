d8257fb65cad1ee80eacf03de2b3dd87fe263553
Giovanni Forma <bluehairguyiscool@gmail.com>
2023-12-28 20:25:25 -0800

    reset repo

--- src/main.c
+++ src/main.c
@@ -1,20 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-struct cat {
-    int age;
-    int weight;
-    char* name;
-};
-
-void set_weight(struct cat* kitty, int new_weight) {
-    kitty->weight = new_weight;
-}
 
 int main() {
-    struct cat kitty = {.age=8, .name="Xena"};
-    set_weight(&kitty, 17);
-
-    printf("Age: %d, Weight: %d, Name: %s", kitty.age, kitty.weight, kitty.name); // Age: 8, Weight: 17, Name: Xena
+    printf("Hello World!\n");
     return 0;
 }
